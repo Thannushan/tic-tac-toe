3297fe305d9e49c7ec86e5e1b17bdd8bb5eedce3
unknown <nthannushan@gmail.com>
2018-11-21 22:38:05 -0500

    initial tictactoe commit

---
+++ tictactoe.py
@@ -0,0 +1,11 @@
+board = [' '] * 10
+
+def print_board():
+    print(board[7],"|",board[8],"|",board[9])
+    print("---------")
+    print(board[4],"|",board[5],"|",board[6])
+    print("---------")
+    print(board[1],"|",board[2],"|",board[3])
+
+
+print_board()
