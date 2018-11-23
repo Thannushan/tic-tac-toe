8dae86146349405fc0f64c2e3bdad64c2ac77e42
unknown <nthannushan@gmail.com>
2018-11-22 20:50:23 -0500

    added function for player to make a move

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,21 @@
-myBoard = [' '] * 10
-
 def print_board(board):
     print(board[7],"|",board[8],"|",board[9])
     print("---------")
     print(board[4],"|",board[5],"|",board[6])
     print("---------")
     print(board[1],"|",board[2],"|",board[3])
+    print('')
 
+def make_move(board,player,move):
+    board[move] = player
+
+
+
+
+myBoard=[' ']*10 
+
+print_board(myBoard)
+
+myBoard[1] = 'X'
 
 print_board(myBoard)
